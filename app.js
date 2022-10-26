134bf7c7acfd007c24007e06b3ff64c78e6358b7
shitalbhosle <shitalbhosleer123@gmail.com>
2022-10-26 19:29:20 +0530

    clipboard added

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var copyText = document.getElementById("input");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value);
+
+  alert("Copied");
+}
